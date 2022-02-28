b4edd0ef7940ce54bf7dd8d0435617579fcad93a
Juel Hossain <juelhossainnn@gmail.com>
2022-03-01 05:35:16 +0600

    lets go

---
+++ main.js
@@ -0,0 +1,8 @@
+const searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener('click', () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    const url = 
+});
