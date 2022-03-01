8a4bcc55644670f075dfe7dc59348d3f05e7847e
Juel Hossain <juelhossainnn@gmail.com>
2022-03-01 06:06:31 +0600

    data fetched

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ searchButton.addEventListener('click', () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
-    const url = 
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
+    
+    
 });
