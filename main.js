3dcca041e193334786d052b01bced265a768c5aa
Juel Hossain <juelhossainnn@gmail.com>
2022-03-01 06:40:24 +0600

    search result is working

--- main.js
+++ main.js
@@ -3,12 +3,25 @@ const searchButton = document.getElementById('search-button');
 searchButton.addEventListener('click', () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     
     fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data));
+        .then(res => res.json())
+        .then(data => showData(data.data));
     
+    const showData = (data) => {
+        const resultBox = document.getElementById('result-box');
+        data.forEach(element => {
+            console.log(element);
+            const div = document.createElement('div');
+            div.classList.add('result');
+            div.innerHTML = `<img src="${element.image}" alt="phone-image">
+              <h2>${element.phone_name}</h2>
+              <p>release date:date</p>
+              <p>Phone Details</p>`
+            resultBox.appendChild(div);
+              
+        });
+        }  
     
 });
